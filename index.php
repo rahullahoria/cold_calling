c0e6ad6b911ce7a487d5b6f06561ca8f375be657
rahullahoria <rahul_lahoria@yahoo.com>
2016-07-19 11:56:58 +0530

    first commit

--- index.php
+++ index.php
@@ -62,7 +62,8 @@ if($_POST['send_sms']){
     $mobile = $_POST['mobile'];
     $mobile = "9599075955";
 
-    $message = "You have just talked to BlueTeam, to Get reliable and trusted domestic service http://goo.gl/545wov ";
+    $message = "You have just talked to BlueTeam, to Get reliable and trusted domestic service like Maid, Cook, Babysitter\n
+                http://goo.gl/545wov ";
     sendSMS($mobile, $message);
 
     //mysqli_query($db_handle, "UPDATE `calling_queue` SET status = 'done' WHERE id = " . $_POST['id']);
